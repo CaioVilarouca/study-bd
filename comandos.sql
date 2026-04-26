5aa978dff4a32c3bb92012c0552cb62e395220ae
Caio <caiovilarouca@gmail.com>
2026-04-25 23:11:42 -0300

    docs (comandos.sql)

--- comandos.sql
+++ comandos.sql
@@ -1,31 +1,40 @@
 create database cadastro 
 default character set utf8
-default collate utf8_general_ci;
+default collate utf8_general_ci; 
 
 use cadastro;
 
 create table pessoas(
-	id int not null auto_increment, -- not null (compo obrigatório) (auto_increment)gerar automaticamente um número único e sequencial a cada nova linha inserida em uma tabela
+	id int not null auto_increment, -- (auto_increment) Gera automaticamente um número único e sequencial
 	nome			varchar(30) 	not null,
     nascimento 		date,
     sexo			enum('M','F'),
-    peso			decimal(5,2), -- são cinco casa e dois pós vírgula
+    peso			decimal(5,2),-- 5 casa e 2 antes da vírgula
     altura			decimal(3,2),
     nacionalidade 	varchar(20) default 'brasil',
-    primary key (id)
+    primary key (id) -- Chave primary
 )default charset = utf8;
 
-insert pessoas (id,nome,nascimento,sexo,peso,altura)
-value
-(default, 'Caio Vilarouca','2004-02-10','M','65.0','1.79'),
-(default, 'Emily Fonseca','2005-03-31', 'F', '45.9','1.59');
+insert into pessoas value  -- ADD registro na tabela
+(default,'Caio Vilarouca','2004-10-02','M','65.2','1.80','Brasileiro'),
+(default,'Emily Fonseca','2005-03-31','F','52.4','1.65','Brasileiro');
 
-insert pessoas value
-(default, 'Herlon Junior','2004-02-10','M','65.0','1.79', 'Areal'),
-(default, 'Larissa Alloano','2005-03-31', 'F', '45.9','1.59', 'Jardim Inga');
+insert into pessoas value  -- ADD registro na tabela
+(default,'Jair Guerra','1098-04-13','M','82.4','1.85',default);
 
-insert pessoas value
-(default, 'Caio Junior','2004-02-10','M','65.0','1.79', default);
+alter table pessoas add profissao varchar(10); -- ADD coluna na tabela
+alter table pessoas add profissao varchar(10) after nome; -- ADD coluna na tabela na posiçao antes de NOME
+alter table pessoas add profissao varchar(10) first; -- ADD coluna na tabela na primeira posição
 
+alter table pessoas modify profissao varchar(25); -- Aumentar o tipo primitivo espaço do varchar 
+
+alter table pessoas drop profissao; -- Drop na coluna da tabela
+drop table pessos; -- Drop na tabela 
+
+alter table pessoas rename to people; -- troca o nome da tabela
+
+
+
+describe pessoas;
 select * from pessoas;
 
